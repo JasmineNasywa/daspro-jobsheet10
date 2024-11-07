43b3aeb61cb82fcc2cdac58d3250104bf33620c9
Jasmine Nasywa Nabilah <jasminenasywa6@gmail.com>
2024-11-07 09:07:59 +0700

    percobaan1 pertanyaan

--- bioskop12.java
+++ bioskop12.java
@@ -24,9 +24,30 @@ public class bioskop12 {
 
         System.out.println(penonton.length);
 
-        for (int i = 0; i < penonton.length; i++) {
+        /*for (int i = 0; i < penonton.length; i++) {
             System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+        }*/
+
+        
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+        
+        System.out.println("Penonton pada baris ke-3: ");
+
+        /*for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }*/
+
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + "; " + String.join(",", penonton [i]));
+        }
+
         }
 
     }
-}
+
