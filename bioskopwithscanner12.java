2ce369a2afbd3638cf200062ae477cdd211cc55c
Jasmine Nasywa Nabilah <jasminenasywa6@gmail.com>
2024-11-07 10:29:09 +0700

    percobaan2 pertanyaan

--- bioskopwithscanner12.java
+++ bioskopwithscanner12.java
@@ -3,27 +3,59 @@ public class bioskopwithscanner12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int baris, kolom;
+        int baris, kolom, menu;
         String nama, next;
 
         String [][] penonton = new String [4][2];
 
         while (true) {
+        System.out.println("Masukkan pilihan menu (1/2/3)");
+        System.out.println("1.Input data penonton \n2.Tampilkan daftar penonton \n3.Exit");
+        System.out.print(">>>");
+        menu = sc.nextInt();
+
+
+       if (menu == 1) {
+        
+       
             System.out.print("Masukkan nama : ");
-            nama = sc.nextLine();
+            nama = sc.next();
             System.out.print("Masukkan baris : ");
             baris = sc.nextInt();
             System.out.print("Masukkan kolom : ");
             kolom = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
-            System.out.print("Input penonton lainnya ? (y/n): ");
+           if (baris <= 4 && kolom <= 2 ) {
+            if ((penonton[baris-1][kolom-1])== null){
+            penonton[baris-1][kolom-1] = nama;}
+            else {
+                System.out.println("Kursi telah terisi.");
+            }
+           } else {
+            System.out.println("Nomor kolom dan baris yang dimasukkan salah.");
+           }
+
+       }
+       else if (menu == 2) {
+        System.out.println("Menampilkan daftar penonton");
+        for (int i = 0; i < penonton.length; i++) {
+            if (penonton[i] == null) {
+                System.out.print("***");
+            }
+            System.out.println("Penonton pada baris ke-" + (i+1) + "; " + String.join(",", penonton [i]));
+        
+       }} else if (menu == 3) {
+        System.out.print("Input penonton lainnya ? (y/n): ");
             next = sc.nextLine();
 
             if (next.equalsIgnoreCase("n")){
                 break;
             }
+       }else {
+        System.out.println("Menu tidak dapat ditemukan.");
+       }
+            
         }
     }
 }
